7c3f985af5d4738fec7e10161358036a9969afae
D'Arcy Saum <darcysaum@gmail.com>
2018-06-22 08:13:20 -0400

    june 2016 updates

--- images.sh
+++ images.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-find . -name "*.jp[e]g" -print0 | xargs -0 -I {} convert -quality 70 -resize x500 {} ../500/{}
-find . -name "*.jp[e]g" -print0 | xargs -0 -I {} convert -quality 70 -resize x300 {} ../300/{}
-find . -name "*.jp[e]g" -print0 | xargs -0 -I {} convert -quality 70 -resize x400 {} ../400/{}
+find . -name "*.jpg" -print0 | xargs -0 -I {} convert -quality 70 -resize x500 {} ../500/{}
+find . -name "*.jpg" -print0 | xargs -0 -I {} convert -quality 70 -resize x300 {} ../300/{}
+find . -name "*.jpg" -print0 | xargs -0 -I {} convert -quality 70 -resize x400 {} ../400/{}
